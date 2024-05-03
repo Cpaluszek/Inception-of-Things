c9b0ba076a2e54bf8d5557ee4e30464d042695d4
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2024-05-03 20:39:08 +0200

    part1 OK

--- p1/setup_server.sh
+++ p1/setup_server.sh
@@ -4,6 +4,8 @@ GREEN="\033[32m"
 RED="\033[31m"
 RESET="\033[0m"
 
+IP="192.168.56.110"
+
 # Check if curl is installed, if not, install it
 if ! command -v curl &> /dev/null
 then
@@ -12,14 +14,20 @@ then
     sudo apt-get update && sudo apt-get install -y curl
 fi
 
-export K3s_INSTALLER="--bind-address=192.168.56.110 --https-listen-port 6443"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san cpaluszeS --bind-address=$IP --node-ip=$IP --advertise-address=$IP"
 
-# Download k3s installer
-if curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=$INSTALL_K3S_EXEC sh -; then
+if curl -sfL https://get.k3s.io | sh -; then
     echo -e "${GREEN}K3s MASTER installation SUCCEEDED${RESET}"
 else
     echo -e "${RED}K3s MASTER installation FAILED${RESET}"
 fi
+
+# Check if k3s service is running
+while [ ! -f /var/lib/rancher/k3s/server/node-token ]
+do
+    echo "Waiting for node-token file to be created..."
+    sleep 2
+done
 
 # Copy the vagrant token to the shared folder
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/token
--- p1/setup_worker.sh
+++ p1/setup_worker.sh
@@ -15,8 +15,10 @@ fi
 # Retrieve k3s token from the master node
 K3S_TOKEN=$(cat /vagrant/token)
 
+export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token $K3S_TOKEN --node-ip=192.168.56.111"
+
 # Download k3s installer and install k3s agent
-if curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$K3S_TOKEN sh -; then
+if curl -sfL https://get.k3s.io | sh -; then
     echo -e "${GREEN}K3s AGENT installation SUCCEEDED${RESET}"
 else
     echo -e "${RED}K3s AGENT installation FAILED${RESET}"
