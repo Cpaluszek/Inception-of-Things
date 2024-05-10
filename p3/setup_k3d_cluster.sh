87b26a357685a592c72433273990dd8b3df84d49
Clement <paluszek.clement@gmail.com>
2024-05-10 11:02:57 +0200

    argocd server setup ok

--- p3/setup_k3d_cluster.sh
+++ p3/setup_k3d_cluster.sh
@@ -20,8 +20,31 @@ create_cluster() {
 }
 
 setup_argo() {
+    echo -e "${GREEN} Installing argocd...${RESET}"
     kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+    echo -e "${GREEN} Downloading argocd cli...${RESET}"
+    curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+    sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+    rm argocd-linux-amd64
+
+    echo -e "${GREEN} Exposing argocd API Server...${RESET}"
+    kubectl port-forward svc/argocd-server -n argocd 8080:443 &>/dev/null &
+    sudo netstat -tulpn | grep :8080
+
+    ARGO_PSW=$(argocd admin initial-password -n argocd)
+    echo -e "${GREEN} argocd admin password: ${ARGO_PSW}${RESET}"
+
+    echo -e "${GREEN} Login to argocd server...${RESET}"
+    argocd login localhost:8080 --insecure --username admin --password SHaFgJ1ltuYLv6GJ
+}
+
+create_argocd_app() {
+    echo -e "${GREEN} Creating argocd application...${RESET}"
+
 }
 
 create_cluster
 setup_argo
+create_argocd_app
+
